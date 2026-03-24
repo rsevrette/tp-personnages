e63012bdfc025191814a653dfb89b2fef816e2da
rsevrette <romain.sevrette@edouardgand.fr>
2026-03-24 08:19:37 +0100

    Test limitation polymorphisme

--- Program.cs
+++ Program.cs
@@ -26,13 +26,14 @@ class Program
         magicien1.LancerSort();
 
         //Partie 7 — Polymorphisme
+        Console.WriteLine();
         List<Personnage> personnages = new List<Personnage>();
         personnages.Add(new Guerrier("Arthur", 100, 20));
         personnages.Add(new Magicien("Merlin", 80, 50));
-
         foreach (Personnage p in personnages)
         {
             p.Afficher();
+            p.Attaquer();
         }
     }
 }
